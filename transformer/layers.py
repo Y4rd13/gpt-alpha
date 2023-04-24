2d46fa593e12f50095785bde5cc302637cd8feab
Benny Serra <benjaserrau@gmail.com>
2023-04-23 20:47:36 -0400

    refactorization of the encoder for multihead-attention, error in shape for matmul

--- transformer/layers.py
+++ transformer/layers.py
@@ -5,16 +5,79 @@ class LinearLayer:
                  input_dim: int, # input dimension of the layer (number of neurons in the previous layer)
                  output_dim: int # output dimension of the layer (number of neurons in the current layer)
                  ):
-        # initialize weight and bias randomly and using a normal distribution
-        self.weight = np.random.rand(input_dim, output_dim)
-        #self.bias = rand(1, output_dim)
-        #self.scale = np.sqrt(output_dim)
+        # initialize weight randomly and using a normal distribution
+        weight_size = (input_dim, output_dim)
+        weight_total_elements = np.prod(weight_size)
+        self.weight = np.random.rand(weight_total_elements).reshape(weight_size)
         
     def forward(self, x):
         # Calculate dot product between the input (x) and the weight (w)
+        print(f'x shape: {x.shape}')
+        print(f'weight shape: {self.weight.shape}')
         output = np.matmul(x, self.weight.T)
+        print(f'output shape: {output.shape}')
+        print('-'*50)
 
+        return output
+class ScaledDotProduct(LinearLayer):
+    def  __init__(self, positional_embedding, len_input_text, d_model, output_dim, mask=None, *args, **kwargs) -> None:
+        self.positional_embedding = positional_embedding
+        self.input_dim = len_input_text
+        self.output_dim = output_dim
+        self.num_heads = d_model
+        self.mask = mask
+
+        super().__init__(self.input_dim, self.output_dim)
+
+        # Initialize matrix for queries, keys, values, and output
+        self.Wq = LinearLayer(self.input_dim, self.output_dim)
+        self.Wk = LinearLayer(self.input_dim, self.output_dim)
+        self.Wv = LinearLayer(self.input_dim, self.output_dim)
+        self.Wo = LinearLayer(self.input_dim, self.output_dim)
+
+    def forward(self):
+        query = self.Wq.forward(self.positional_embedding)
+        key = self.Wk.forward(self.positional_embedding)
+        value = self.Wv.forward(self.positional_embedding)
+
+        # Calculate attention scores
+        output = np.matmul(query, key.T)
+        # output /= output/np.sqrt(shape(output)[0], shape(output)[1])
+        
         # Normalization of the output scores
-        #scores = output / self.scale
+        scores = output / np.sqrt(self.output_dim)
+
+        if self.mask is not None: # que chucha?
+            scores += -1e9 * self.mask
+
+        attn_filter = self.softmax(x=scores)
+
+        # Apply attention to values
+        output = np.matmul(attn_filter, value.T) # value tiene que estar transpuesto?
+        
+        return output
+
+    def softmax(self, x):
+        e_x = np.exp(x - np.max(x))
+        return e_x / e_x.sum(axis=-1, keepdims=True) # keepdims to keep same shape and axis=-1 to sum over last axis
+    
+class MultiHeadAttention(ScaledDotProduct):
+    def __init__(self, positional_embedding, len_input_text, d_model, output_dim, mask=None, *args, **kwargs) -> None:
+        super().__init__(positional_embedding, len_input_text, d_model, output_dim, mask, *args, **kwargs)
+        self.len_input_text = len_input_text
+        self.d_model = d_model
+    
+    def forward(self):
+        # Create multi-head attention object with Q, K, V, and output weights
+        scaled_dot_prod = ScaledDotProduct(positional_embedding=self.positional_embedding, len_input_text=self.len_input_text, d_model=self.d_model, output_dim=self.d_model)
+
+        # Apply multi-head attention
+        filtered_value = [scaled_dot_prod.forward(), scaled_dot_prod.forward()] #* self.heads
+
+        # Concatenate
+        concat_value = np.concatenate(filtered_value, axis=0) # axis=0 to concatenate vertically and axis=1 to concatenate horizontally
+        
+        # Apply linear layer
+        output = scaled_dot_prod.Wo.forward(concat_value)
 
         return output
--- transformer/model.py
+++ transformer/model.py
@@ -1,60 +1,7 @@
 import numpy as np
-from layers import LinearLayer
+from layers import LinearLayer, MultiHeadAttention
 class Transformer:
     pass
-
-class MultiHeadAttention(LinearLayer):
-    def  __init__(self, positional_embedding, len_input_text, d_model, output_dim, mask=None, *args, **kwargs) -> None:
-        self.positional_embedding = positional_embedding
-        self.input_dim = len_input_text
-        self.output_dim = output_dim
-        self.num_heads = d_model
-
-        super().__init__(input_dim=self.input_dim, output_dim=self.output_dim) # no estoy seguro del output_dim
-
-        # Initialize matrix for queries, keys, values, and output
-        self.Wq = LinearLayer(input_dim=self.input_dim, output_dim=self.output_dim)
-        self.Wk = LinearLayer(input_dim=self.input_dim, output_dim=self.output_dim)
-        self.Wv = LinearLayer(input_dim=self.input_dim, output_dim=self.output_dim)
-        self.Wo = LinearLayer(input_dim=self.input_dim, output_dim=self.output_dim)
-    
-    def forward(self):
-        # Attention(Q, K, V) = softmax(QK^T / sqrt(d_k))V
-        filtered_value = self.scaled_dot_product_attention()
-
-        # Concatenate
-        concat_value = np.concatenate(filtered_value, axis=1)
-
-        # Apply linear layer
-        output = self.Wo.forward(concat_value)
-
-        return output
-
-    def scaled_dot_product_attention(self):
-        query = self.Wq.forward(self.positional_embedding)
-        key = self.Wk.forward(self.positional_embedding)
-        value = self.Wv.forward(self.positional_embedding)
-
-        # Calculate attention scores
-        output = np.matmul(query, key.T)
-        # output /= output/np.sqrt(shape(output)[0], shape(output)[1])
-        
-        # Normalization of the output scores
-        scores = output / np.sqrt(self.output_dim)
-
-        if self.mask is not None: # que chucha?
-            scores += -1e9 * self.mask
-
-        attn_filter = self.softmax(x=scores)
-
-        # Apply attention to values
-        output = np.matmul(attn_filter, value) # value tiene que estar transpuesto?
-        
-        return output
-
-    def softmax(self, x):
-        e_x = np.exp(x - np.max(x))
-        return e_x / e_x.sum(axis=-1, keepdims=True) # keepdims to keep same shape and axis=-1 to sum over last axis
     
 class Encoder(MultiHeadAttention):
     def __init__(self, d_model: int, heads: int, *args, **kwargs):
@@ -62,12 +9,13 @@ class Encoder(MultiHeadAttention):
         self.heads = heads
     
     def execute(self, input_text: str):
+        # Get initial embedding and positional encoding
         initial_embedding = self.__get_rand_embedding(input_text)
-        positional_encoding = self.__get_positional_encoding(initial_embedding=initial_embedding)
+        positional_encoding = self.__get_positional_encoding()
         positional_embedding = np.add(initial_embedding, positional_encoding)
-        multi_head_attention = self.heads * [MultiHeadAttention(positional_embedding=positional_embedding, len_input_text=self.len_input_text, d_model=self.d_model, output_dim=self.d_model)]
-        return multi_head_attention
-    
+        multi_head_attn = MultiHeadAttention(positional_embedding=positional_embedding, len_input_text=self.len_input_text, d_model=self.d_model, output_dim=self.d_model).forward()
+        return multi_head_attn
+
     def __get_positional_encoding(self):
         embedding_dim = self.d_model
         pos_enc = np.zeros((self.len_input_text, self.d_model))
@@ -89,5 +37,6 @@ class Encoder(MultiHeadAttention):
 
 if __name__ == '__main__':
     encoder = Encoder(d_model=5, heads=3)
-    encoder.execute(input_text='hello world')
+    x = encoder.execute(input_text='hello world')
+    print(x)
     print('Success!')
