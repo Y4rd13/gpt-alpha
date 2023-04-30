c33298cd846901c9c3c39c9333dc9bfd60bcfa29
Benny Serra <benjaserrau@gmail.com>
2023-04-30 03:13:09 -0400

    adding activation function

---
+++ transformer/activations.py
@@ -0,0 +1,133 @@
+import numpy as np
+
+class Activation:
+    def __init__(self):
+        self.name = self.__class__.__name__
+
+    def __call__(self, x):
+        return self.forward(x)
+    
+    def forward(self, x):
+        raise NotImplementedError
+    
+    def backward(self, x):
+        raise NotImplementedError
+    
+class ReLU(Activation):
+    def forward(self, x):
+        return np.maximum(0, x)
+    
+    def backward(self, x):
+        return np.where(x > 0, 1, 0)
+    
+class Sigmoid(Activation):
+    def forward(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def backward(self, x):
+        return self.forward(x) * (1 - self.forward(x))
+    
+class Tanh(Activation):
+    def forward(self, x):
+        return np.tanh(x)
+    
+    def backward(self, x):
+        return 1 - np.square(self.forward(x))
+
+class Softmax(Activation):
+    def __init__(self, axis=-1, keepdims: bool = True):
+        super().__init__()
+        self.name = "Softmax"
+        self.axis = axis
+        self.keepdims = keepdims
+
+    def forward(self, x):
+        return self.softmax(x)
+    
+    def backward(self, x):
+        return self.softmax(x) * (1 - self.softmax(x))
+
+    def softmax(self, x):
+        e_x = np.exp(x - np.max(x, axis=self.axis, keepdims=self.keepdims))
+        return e_x / e_x.sum(axis=self.axis, keepdims=self.keepdims) # keepdims to keep same shape and axis=-1 to sum over last axis
+    
+class GELU(Activation):
+    def forward(self, x):
+        return 0.5 * x * (1 + np.tanh(np.sqrt(2 / np.pi) * (x + 0.044715 * np.power(x, 3))))
+    
+    def backward(self, x):
+        return 0.5 * (1 + np.tanh(np.sqrt(2 / np.pi) * (x + 0.044715 * np.power(x, 3)))) + 0.5 * x * (1 - np.square(np.tanh(np.sqrt(2 / np.pi) * (x + 0.044715 * np.power(x, 3))))) * np.sqrt(2 / np.pi) * (1 + 0.134145 * np.power(x, 2))
+    
+class LeakyReLU(Activation):
+    def __init__(self, alpha: float = 0.2):
+        self.alpha = alpha
+    
+    def forward(self, x):
+        return np.where(x > 0, x, x * self.alpha)
+    
+    def backward(self, x):
+        return np.where(x > 0, 1, self.alpha)
+    
+class ELU(Activation):
+    def __init__(self, alpha: float = 1.0):
+        self.alpha = alpha
+    
+    def forward(self, x):
+        return np.where(x > 0, x, self.alpha * (np.exp(x) - 1))
+    
+    def backward(self, x):
+        return np.where(x > 0, 1, self.alpha * np.exp(x))
+    
+class SELU(Activation):
+    def __init__(self, alpha: float = 1.6732632423543772848170429916717, scale: float = 1.0507009873554804934193349852946):
+        self.alpha = alpha
+        self.scale = scale
+    
+    def forward(self, x):
+        return self.scale * np.where(x > 0, x, self.alpha * (np.exp(x) - 1))
+    
+    def backward(self, x):
+        return self.scale * np.where(x > 0, 1, self.alpha * np.exp(x))
+    
+class Softplus(Activation):
+    def forward(self, x):
+        return np.log(1 + np.exp(x))
+    
+    def backward(self, x):
+        return 1 / (1 + np.exp(-x))
+    
+class Softsign(Activation):
+    def forward(self, x):
+        return x / (1 + np.abs(x))
+    
+    def backward(self, x):
+        return 1 / np.square(1 + np.abs(x))
+    
+class Mish(Activation):
+    def forward(self, x):
+        return x * np.tanh(np.log(1 + np.exp(x)))
+    
+    def backward(self, x):
+        return np.tanh(np.log(1 + np.exp(x))) + x * (1 - np.square(np.tanh(np.log(1 + np.exp(x))))) * (1 / (1 + np.exp(-x)))
+    
+class Swish(Activation):
+    def forward(self, x):
+        return x / (1 + np.exp(-x))
+    
+    def backward(self, x):
+        return (np.exp(-x) * (x + 1) + 1) / np.square(1 + np.exp(-x))
+    
+class HardSigmoid(Activation):
+    def forward(self, x):
+        return np.clip((x + 3) / 6, 0, 1)
+    
+    def backward(self, x):
+        return np.where((x > -3) & (x < 3), 1 / 6, 0)
+    
+class HardSwish(Activation):
+    def forward(self, x):
+        return x * np.clip((x + 3) / 6, 0, 1)
+    
+    def backward(self, x):
+        return np.where((x > -3) & (x < 3), (x + 3) / 3, 0)
+
--- transformer/layers.py
+++ transformer/layers.py
@@ -2,18 +2,75 @@
 https://nn.labml.ai/normalization/layer_norm/index.html
 '''
 import numpy as np
+import activations
+from typing import List
 
-class PositionalEmbedding:
+class Layer:
+    def __init__(self, name: str = None, dtype=None, trainable=True,
+                 input_spec=None, **kwargs):
+        self.name = name
+        self.dtype = dtype
+        self.trainable = trainable
+        self.input_spec = input_spec
+
+        self._added_weight = []
+
+    def __call__(self, inputs, *args, **kwargs):
+        raise NotImplementedError
+
+    def add_weight(self, name, shape, dtype=None, initializer=None, trainable=True,
+                   getter=None, **kwargs):
+        # A lightweight alternative to tf.Variable.
+        # It simply adds a weight variable to the layer.
+        if trainable:
+            self._added_weight.append(name)
+        weight = np.random.randn(*shape)
+        setattr(self, name, weight)
+        return weight
+
+    @property
+    def weights(self) -> List[np.ndarray]:
+        # Returns the weights of the layer.
+        return [self.trainable_weights, self.non_trainable_weights]
+
+    @property
+    def trainable_weights(self) -> List[np.ndarray]:
+        # Returns the trainable weights of the layer.
+        return [getattr(self, weight) for weight in self._added_weight]
+
+    @property
+    def non_trainable_weights(self) -> List[np.ndarray]:
+        # Returns the non trainable weights of the layer.
+        return []
+
+    def get_weights(self) -> List[np.ndarray]:
+        # Returns the current weights of the layer.
+        return self.weights
+
+    def set_weights(self, weights: List[np.ndarray]) -> None:
+        # Sets the weights of the layer.
+        assert len(weights) == 2, "Expected two arrays of weights, got {}".format(len(weights))
+        trainable_weights, non_trainable_weights = weights
+        assert len(trainable_weights) == len(self.trainable_weights), \
+            "Expected {} trainable weights, got {}".format(len(self.trainable_weights), len(trainable_weights))
+        for i, weight in enumerate(trainable_weights):
+            setattr(self, self._added_weight[i], weight)
+
+class PositionalEmbedding(Layer):
     def __init__(self, d_model: int, input_sequence_length: int):
+        super().__init__()
         self.d_model = d_model
         self.input_sequence_length = input_sequence_length
-        self.n = 10000 # Constant for the sinusoidal functions: max number of words in a sentence used in Attention is All You Need paper.
-    
-    def call(self):
+        self.n = 10000  # Constant for the sinusoidal functions: max number of words in a sentence used in Attention is All You Need paper.
+
+    def build(self, input_shape):
+        super().build(input_shape)
+
+    def call(self, inputs=None):
         # Get initial embedding and positional encoding
-        initial_embedding = self.__get_rand_embedding() # get random initial embedding
-        positional_encoding = self.__get_positional_encoding() # get positional encoding
-        positional_embedding = np.add(initial_embedding, positional_encoding) # add positional encoding to initial embedding
+        initial_embedding = self.__get_rand_embedding()  # get random initial embedding
+        positional_encoding = self.__get_positional_encoding()  # get positional encoding
+        positional_embedding = np.add(initial_embedding, positional_encoding)  # add positional encoding to initial embedding
         return positional_embedding
 
     def __get_positional_encoding(self):
@@ -33,23 +90,23 @@ class PositionalEmbedding:
         initial_embedding = np.random.randn(self.input_sequence_length, self.d_model)
         return initial_embedding
 
-class LinearLayer:
-    def __init__(self, 
-                 input_dim: int, # input dimension of the layer (number of neurons in the previous layer)
-                 output_dim: int, # output dimension of the layer (number of neurons in the current layer)
-                 input_size: int = None # input size of the layer (number of neurons in the current layer)
-                 ):
+class Linear(Layer):
+    def __init__(self, input_dim: int, output_dim: int, input_size: int = None):
+        super().__init__()
+
         # initialize weight randomly and using a normal distribution
         weight_size = (input_dim or input_size, output_dim)
-        weight_total_elements = np.prod(weight_size)
-        self.weight = np.random.randn(weight_total_elements).reshape(weight_size)
-        
+        self.weight = self.add_weight(name='weight', shape=weight_size)
+
     def forward(self, x):
         # Calculate dot product between the input (x) and the weight (w)
         output = np.matmul(x, self.weight.T)
-
         return output
-class ScaledDotProduct(LinearLayer):
+
+    def __call__(self, inputs):
+        return self.forward(inputs)
+
+class ScaledDotProduct(Linear):
     def  __init__(self, positional_encoding, input_sequence_length, heads, output_dim, mask=None):
         self.positional_encoding = positional_encoding
         self.input_dim = input_sequence_length
@@ -61,10 +118,10 @@ class ScaledDotProduct(LinearLayer):
         super().__init__(self.input_dim, self.output_dim)
 
         # Initialize matrix for queries, keys, values, and output
-        self.Wq = LinearLayer(self.input_dim, self.output_dim)
-        self.Wk = LinearLayer(self.input_dim, self.output_dim)
-        self.Wv = LinearLayer(self.input_dim, self.output_dim)
-        self.Wo = LinearLayer(self.heads * self.output_dim, self.output_dim, input_size=self.input_dim)
+        self.Wq = Linear(self.input_dim, self.output_dim)
+        self.Wk = Linear(self.input_dim, self.output_dim)
+        self.Wv = Linear(self.input_dim, self.output_dim)
+        self.Wo = Linear(self.heads * self.output_dim, self.output_dim, input_size=self.input_dim)
 
     def forward(self):
         query = self.Wq.forward(self.positional_encoding)
@@ -75,21 +132,17 @@ class ScaledDotProduct(LinearLayer):
         output = np.matmul(query, key.T)
         
         # Normalization of the output scores
-        scores = output / np.sqrt(self.output_dim // self.heads) # self.output_dim/self.num_heads? 
+        scores = output / np.sqrt(self.d_k) # self.output_dim/self.num_heads? 
 
         if self.mask is not None:
             scores += -1e9 * self.mask
 
-        attn_filter = self.softmax(x=scores)
+        attn_filter = activations.Softmax(axis=-1, keepdims=True).forward(scores)
 
         # Apply attention to values
         output = np.matmul(attn_filter, value[:, :self.d_k])
         
         return output
-
-    def softmax(self, x):
-        e_x = np.exp(x - np.max(x))
-        return e_x / e_x.sum(axis=-1, keepdims=True) # keepdims to keep same shape and axis=-1 to sum over last axis
     
 class MultiHeadAttention(ScaledDotProduct):
     def __init__(self,
@@ -131,7 +184,6 @@ class AddAndNorm:
     def __init__(self, input_dim: int):
         self.input_dim = input_dim
         self.epsilon = 1e-8 # 1e-8 to avoid division by zero
-        
 
     def forward(self, positional_encoding, multi_head_output, residual):
         # assert self.normalized_shape == x.shape[-len(self.normalized_shape):]
@@ -156,30 +208,19 @@ class AddAndNorm:
 
         return output
 
-class FeedForward(LinearLayer):
-    def __init__(self, input_dim: int, output_dim: int, activation: str):
-        self.linear_layer_1 = LinearLayer(input_dim, output_dim)
-        self.linear_layer_2 = LinearLayer(input_dim, output_dim)
-        self.activation = activation
+class FeedForward(Linear):
+    def __init__(self, input_dim: int, output_dim: int):
+        self.linear_layer_1 = Linear(input_dim, output_dim)
+        self.linear_layer_2 = Linear(input_dim, output_dim)
 
     def forward(self, x):
         # Apply linear layer
         linear_layer_1 = self.linear_layer_1.forward(x)
 
-        # Apply activation function 
-        linear_layer_1_act = self.activation_layer(linear_layer_1)
+        # Apply ReLu activation function 
+        linear_layer_1_act = activations.ReLU().forward(linear_layer_1)
 
         # Apply linear layer
         linear_layer_2 = self.linear_layer_2.forward(linear_layer_1_act)
 
         return linear_layer_2
-    
-    def activation_layer(self, x):
-        if self.activation == 'relu':
-            out = np.maximum(0, x)
-        elif self.activation == 'gelu':
-            out = 0.5 * x * (1 + np.tanh(np.sqrt(2 / np.pi) * (x + 0.044715 * np.power(x, 3))))
-        elif self.activation == 'tanh':
-            out = np.tanh(x)
-
-        return out
