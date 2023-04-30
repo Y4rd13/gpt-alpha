7e1454a59fd83fe455269cf96c07e1afbe228bb4
Benny Serra <benjaserrau@gmail.com>
2023-04-30 05:17:25 -0400

    removing unused argument in main

--- transformer/main.py
+++ transformer/main.py
@@ -6,7 +6,6 @@ def main():
     parser.add_argument('-F', '--input_file', type=str, default='transformer/input_text.txt', help='Path to the input text file')
     parser.add_argument('-H', '--heads', type=int, default=2, help='Number of attention heads')
     parser.add_argument('-D', '--d_model', type=int, default=4, help='Size of the model')
-    parser.add_argument('-i', '--iter', type=int, default=10, help='Number of iterations for testing mode')
     parser.add_argument('--plot_posemb', default=False, action='store_true', help='Plot the positional embedding')
 
     args = parser.parse_args()
